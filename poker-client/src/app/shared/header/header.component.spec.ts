ae938eff926ec525f9557c312251e2bb549b47c6
Jon Gourley <jon.gourley@caseware.com>
2020-06-28 14:47:38 -0400

    Fix unit test

--- poker-client/src/app/shared/header/header.component.spec.ts
+++ poker-client/src/app/shared/header/header.component.spec.ts
@@ -2,7 +2,6 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {HeaderComponent} from './header.component';
 import {RouterTestingModule} from '@angular/router/testing';
 import {MockStore, provideMockStore} from '@ngrx/store/testing';
-import {AppState} from '../models/app-state.model';
 import {TopBarComponent} from '../top-bar/top-bar.component';
 import {TopBarItemComponent} from '../top-bar/top-bar-item/top-bar-item.component';
 import {TopBarInfoComponent} from '../top-bar/top-bar-info/top-bar-info.component';
@@ -11,12 +10,6 @@ describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
   let store: MockStore;
-  const initialState: AppState = {
-    ready: false,
-    currentPage: '',
-    authenticated: false,
-    inGame: false
-  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
