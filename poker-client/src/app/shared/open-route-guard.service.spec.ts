ec274d3c2c576ddfffc9ab36c571e9090ef3e7b1
Jon Gourley <jon.gourley@caseware.com>
2020-07-11 21:04:05 -0400

    Fix unit test

--- poker-client/src/app/shared/open-route-guard.service.spec.ts
+++ poker-client/src/app/shared/open-route-guard.service.spec.ts
@@ -1,12 +1,17 @@
 import {TestBed} from '@angular/core/testing';
 
 import {OpenRouteGuardService} from './open-route-guard.service';
+import {provideMockStore} from '@ngrx/store/testing';
 
 describe('OpenRouteGuardService', () => {
   let service: OpenRouteGuardService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideMockStore()
+      ]
+    });
     service = TestBed.inject(OpenRouteGuardService);
   });
 
